4ce53aeabcd5bd201ad8bbddb90c3d5c009c2e9b
LuAndre49 <134673214+LuAndre49@users.noreply.github.com>
2024-06-04 10:38:38 +0800

    corrected the array indexing

--- nasm1/non_SIMD.asm
+++ nasm1/non_SIMD.asm
@@ -1,5 +1,7 @@
 
 section .data
+A dq 0
+B dq 0
 sdot dq 0
 section .text
 bits 64
@@ -7,15 +9,18 @@ default rel
 
 global main
 main:
- 
-    ;for getting dot product
-    XOR RSI, RSI
-    XOR R14, R14
-    MOV R15, count
+    XOR RSI, RSI    ;initialize RSI (which will be used to index the arrays) to 0
+    XOR R14, R14    ;initialize R14 (which is the dot product accumulator) to 0
+    MOV R15, count   
+    MOV [A], RCX    ;save array pointer of 1st array in A
+    MOV [B], RDX    ;save array pointer of 2nd array in B
+    
+    MOV RAX, [A]
+    MOV RBX, [B]
     
 DOTPRODUCT:
-    MOV R12, [A + 8*RSI]
-    MOV R13, [B + 8*RSI]
+    MOV R12, [RAX + 8*RSI]
+    MOV R13, [RBX + 8*RSI]
     
     IMUL R12, R13
     
