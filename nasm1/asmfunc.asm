8324b4a0e19364007245fe29a0b4d8323875379b
adrielteng <adrielteng@gmail.com>
2024-06-04 10:50:11 +0800

    fixed addressing

--- nasm1/asmfunc.asm
+++ nasm1/asmfunc.asm
@@ -2,30 +2,34 @@ section .data
 A dq 0
 B dq 0
 sdot dq 0
+message db "Dot product = %lld", 13,10,0
 section .text
 bits 64
 default rel
+extern printf
 global nonavx
 nonavx:
  
     ;for getting dot product
     XOR RSI, RSI
     XOR R14, R14
+    MOV R15, RCX
     MOV [A], RDX
     MOV [B], R8
-    MOV R15, RCX
     
 DOTPRODUCT:
-    MOV RBX, RSI
-    IMUL RBX, 8
     MOV RAX, [A]
-    ADD RAX, RBX
-    MOV R12, [RAX]
-    MOV RBX, RSI
-    IMUL RBX, 8
+    MOV R12, [RAX + 8*RSI]
+
+    SUB RSP, 8*5
+    LEA RCX, [message]
+    MOV RDX, R12
+    CALL printf
+    ADD RSP, 8*5
+
+
     MOV RAX, [B]
-    ADD RAX, RBX
-    MOV R13, [RAX]
+    MOV R13, [RAX + 8*RSI]
     
     IMUL R12, R13
     
@@ -36,5 +40,4 @@ DOTPRODUCT:
     
     MOV [sdot], R14
     MOV RAX, [sdot]
-    xor rax, rax
     ret
--- nasm1/main.c
+++ nasm1/main.c
@@ -6,9 +6,19 @@
 int nonavx(int count, int* A, int* b);
 
 int main() {
-	int count;
-	int* A, B;
-	nonavx(count,A,B);
-
+	int count = 5,ans;
+	int *A, *B;
+	A = (int*)malloc((size_t)(8 * count));
+	B = (int*)malloc((size_t)(8 * count));
+	for (int i = 0; i < count; i++)
+	{
+		A[i] = i + 1;
+		B[i] = i + 1;
+	}
+	ans=nonavx(count,&A,&B);
+	for (int i = 0; i < count; i++)
+	{
+		printf("%d", ans);
+	}
 	return 0;
 }
